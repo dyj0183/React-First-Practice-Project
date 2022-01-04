e43abc59544a83f225d1a3c0110975faeca817ab
YuChunRaceEntry <yuchun.raceentry@gmail.com>
2022-01-04 15:41:57 -0700

    added more components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 // use the css file we created
 import "./ExpenseItem.css";
+// use ExpenseDate component
+import ExpenseDate from "./ExpenseDate";
 
 // a component in React is just a sepcial kind of function
 // props is an object that contains key value pairs data passed in from App.js
 function ExpenseItem(props) {
-
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">$ {props.amount}</div>
