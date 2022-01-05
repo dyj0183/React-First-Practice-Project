d603f6b58c22f68e9e3982062fd96e85f866219e
YuChunRaceEntry <yuchun.raceentry@gmail.com>
2022-01-04 18:41:57 -0700

    added Card

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+
+function Card(props) {
+    // we need to make sure other classes names will be applied as well such as expense-item 
+    const classes = 'card ' + props.className;
+
+    return (
+        // children will be the codes that between opening and closing Card tag, Ex: in Expenses.js
+        <div className={classes}>{props.children}</div>
+    );
+}
+
+export default Card
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,18 +2,19 @@
 import "./ExpenseItem.css";
 // use ExpenseDate component
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 // a component in React is just a sepcial kind of function
 // props is an object that contains key value pairs data passed in from App.js
 function ExpenseItem(props) {
 	return (
-		<div className="expense-item">
+		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">$ {props.amount}</div>
 			</div>
-		</div>
+		</Card>
 	);
 }
 
