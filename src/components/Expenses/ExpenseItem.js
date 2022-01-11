b1705dfd49a536152bab472ed5a947e4190ff61a
Yu Chun Tung <dyj0183@byui.edu>
2022-01-11 11:35:02 -0700

    added two way data binding & cleaned out input after form submission

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 // import React is not necessary in this case but we still can do it
-import React, { useState } from 'react';
+import React from 'react';
 
 // use the css file we created
 import "./ExpenseItem.css";
@@ -12,22 +12,15 @@ import Note from "./ExpenseNote";
 // a component in React is just a sepcial kind of function
 // props is an object that contains key value pairs data passed in from App.js
 const ExpenseItem = (props) => {
-	// useState will call this component (function) again when it was executed
-	const [title, setTitle] = useState(props.title);
-
-	const clickHandler = () => {
-		setTitle("New Title!");
-	}
 
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<Note note={props.note}></Note>
 				<div className="expense-item__price">$ {props.amount}</div>
 			</div>
-			<button onClick={clickHandler}>Update Title</button>
 		</Card>
 	);
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,65 +3,71 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-
-    const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredTitle, setEnteredTitle] = useState("");
 	// when the change event occurs, we automatically get the "event"
 	const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
+		setEnteredTitle(event.target.value);
 	};
 
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-    }
+	const [enteredAmount, setEnteredAmount] = useState("");
+	const amountChangeHandler = (event) => {
+		setEnteredAmount(event.target.value);
+	};
 
-    const [enteredDate, setEnteredDate] = useState('');
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-    }
+	const [enteredDate, setEnteredDate] = useState("");
+	const dateChangeHandler = (event) => {
+		setEnteredDate(event.target.value);
+	};
 
-    const submitHandler = (event) => {
-        // prevent the form from being sending to the server, so the page will NOT be reloaded
-        event.preventDefault();
+	const submitHandler = (event) => {
+		// prevent the form from being sending to the server, so the page will NOT be reloaded
+		event.preventDefault();
 
-        // handle it by using js here
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate),
-        };
+		// handle it by using js here
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		};
 
-        console.log(expenseData);
-    }
+		console.log(expenseData);
+		// set all the values back to empty after submitting the form
+		setEnteredTitle("");
+		setEnteredAmount("");
+		setEnteredDate("");
+	};
 
 	return (
 		<form onSubmit={submitHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
-                <div>Entered Title: {enteredTitle}</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
 					<input
 						type="number"
+						value={enteredAmount}
 						min="0.01"
 						step="0.01"
 						onChange={amountChangeHandler}
 					/>
 				</div>
-                <div>Entered Amount: {enteredAmount}</div>
 				<div className="new-expense__control">
 					<label>Date</label>
 					<input
 						type="date"
+						value={enteredDate}
 						min="2019-01-01"
 						max="2022-12-31"
 						onChange={dateChangeHandler}
 					/>
 				</div>
-                <div>Entered Date: {enteredDate}</div>
 			</div>
 			<div className="new-expense__actions">
 				<button type="submit">Add Expense</button>
