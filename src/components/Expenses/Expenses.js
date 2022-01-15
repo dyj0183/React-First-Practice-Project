884bc1503cabd40281ebaafa0651c65a1478eb42
Yu Chun Tung <dyj0183@byui.edu>
2022-01-15 14:11:19 -0700

    used JS map function to render expense data dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Expenses = (props) => {
 	// set default year to be 2020
@@ -12,35 +12,26 @@ const Expenses = (props) => {
 	const filterYearHandler = (selectedFilterYear) => {
 		setFilteredYear(selectedFilterYear);
 		console.log(selectedFilterYear);
-	}
+	};
+
+	console.log(props.expenses);
 
 	return (
 		<Card className="expenses">
-			<ExpensesFilter selected={filteredYear} onFilterYear={filterYearHandler}/>
-			<ExpenseItem
-				title={props.expenses[0].title}
-				amount={props.expenses[0].amount}
-				date={props.expenses[0].date}
-				note={props.expenses[0].note}
-			></ExpenseItem>
-			<ExpenseItem
-				title={props.expenses[1].title}
-				amount={props.expenses[1].amount}
-				date={props.expenses[1].date}
-				note={props.expenses[1].note}
-			></ExpenseItem>
-			<ExpenseItem
-				title={props.expenses[2].title}
-				amount={props.expenses[2].amount}
-				date={props.expenses[2].date}
-				note={props.expenses[2].note}
-			></ExpenseItem>
-			<ExpenseItem
-				title={props.expenses[3].title}
-				amount={props.expenses[3].amount}
-				date={props.expenses[3].date}
-				note={props.expenses[3].note}
-			></ExpenseItem>
+			<ExpensesFilter
+				selected={filteredYear}
+				onFilterYear={filterYearHandler}
+			/>
+
+			{props.expenses.map((expense) => (
+				<ExpenseItem
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+					note={expense.note}
+				></ExpenseItem>
+			))}
+
 		</Card>
 	);
 };
