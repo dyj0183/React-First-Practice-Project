6ce9c0087682d9e045a03958809ae9a6ebc36e4e
Yu Chun Tung <dyj0183@byui.edu>
2022-01-17 16:51:02 -0700

    added logic to display and hide the form based on what users click on

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './NewExpense.css';
+import "./NewExpense.css";
 
-import ExpenseForm from './ExpenseForm';
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const newExpenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString(),
-        }
+	const saveExpenseDataHandler = (enteredExpenseData) => {
+		const newExpenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString(),
+		};
 
-        props.onNewExpenseData(newExpenseData);
-    }
+		props.onNewExpenseData(newExpenseData);
+		// hide the form after we add a new expense (submit the form)
+		setIsDisplayForm(false);
+	};
 
-    return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
-        </div>
-    )
-}
+    // set up the state for displaying and hiding form
+	const [isDisplayForm, setIsDisplayForm] = useState(false);
+
+	const displayFormHandler = () => {
+		setIsDisplayForm(true);
+	};
+
+	// if this function is called, means users click on cancel in the form, so we want to hide the form
+	const hideFormHandler = () => {
+		setIsDisplayForm(false);
+	};
+
+	return (
+		<div className="new-expense">
+			{!isDisplayForm && (
+				<button onClick={displayFormHandler}>Add New Expense</button>
+			)}
+			{isDisplayForm && (
+				<ExpenseForm
+					onSaveExpenseData={saveExpenseDataHandler}
+					onHideForm={hideFormHandler}
+				></ExpenseForm>
+			)}
+		</div>
+	);
+};
 
 export default NewExpense;
